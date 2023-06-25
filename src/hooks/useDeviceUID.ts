f30a8b1a6a56657aa2eafa809648cb893f7f0c96
fecore35 <79923560+fecore35@users.noreply.github.com>
2023-06-25 18:58:24 +0300

    fix: dispatch device_uid

--- src/hooks/useDeviceUID.ts
+++ src/hooks/useDeviceUID.ts
@@ -1,8 +1,22 @@
+import { useEffect } from "react";
 import { useSearchParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { useAppDispatch } from "redux/hooks";
+import { getDeviceUID } from "redux/device/device-selectors";
+import * as actions from "redux/device/device-reducer";
 
 const useDeviceUID = () => {
   const [searchParams, setSearchParams] = useSearchParams();
-  const isDeviceUID = searchParams.get("id");
+  const isDeviceUIDStore = useSelector(getDeviceUID);
+  const isDeviceUIDParams = searchParams.get("device_uid");
+  const isDeviceUID = isDeviceUIDParams || isDeviceUIDStore;
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    if (isDeviceUIDParams) {
+      dispatch(actions.setDeviceUID(isDeviceUIDParams));
+    }
+  }, [dispatch, isDeviceUIDParams]);
 
   return { isDeviceUID };
 };
