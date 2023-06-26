18f02e6c1bd98d17f4ef51fc1e5bde63cd4d3445
fecore35 <79923560+fecore35@users.noreply.github.com>
2023-06-26 18:08:00 +0300

    feature: primitive ShopPage

--- src/pages/ShopPage.tsx
+++ src/pages/ShopPage.tsx
@@ -1,9 +1,23 @@
+import Section from "components/Section/Section";
+import Title from "components/Title/Title";
 import { FunctionComponent } from "react";
+import { Link } from "react-router-dom";
 
 interface ShopProps {}
 
 const ShopPage: FunctionComponent<ShopProps> = () => {
-  return <></>;
+  return (
+    <>
+      <Section>
+        <>
+          <Title type="h3">Виберіть зі списку зручний вам Ваш Поштомат</Title>
+          <Link to="/?device_uid=34670a76-6022-4df5-ac1d-68f88589c904">
+            #0017
+          </Link>
+        </>
+      </Section>
+    </>
+  );
 };
 
 export default ShopPage;
