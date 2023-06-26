78e5ac6e46669fffa6255a1a88538f4818be4710
fecore35 <79923560+fecore35@users.noreply.github.com>
2023-06-26 18:05:49 +0300

    fix: CellsSizePage

--- src/pages/CellsSizePage.tsx
+++ src/pages/CellsSizePage.tsx
@@ -1,9 +1,22 @@
-import { FunctionComponent } from "react";
+import CellsList from "components/CellsList/CellsList";
+import { FunctionComponent, useEffect } from "react";
+import { useSelector } from "react-redux";
+import { fetchCellsList } from "redux/cell/cell-operations";
+import { getCellsList, getCurrentCell } from "redux/cell/cell-selectors";
+import { useAppDispatch } from "redux/hooks";
 
 interface CellsSizeProps {}
 
 const CellsSizePage: FunctionComponent<CellsSizeProps> = () => {
-  return <></>;
+  const dispatch = useAppDispatch();
+  const cellsList = useSelector(getCellsList);
+  const currentCell = useSelector(getCurrentCell);
+
+  useEffect(() => {
+    dispatch(fetchCellsList());
+  }, [dispatch]);
+
+  return <CellsList items={cellsList} current={currentCell} />;
 };
 
 export default CellsSizePage;
