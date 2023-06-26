ac5c52f190620526d1823e9ae4d2c15dcf041408
fecore35 <79923560+fecore35@users.noreply.github.com>
2023-06-26 18:06:27 +0300

    fix: hook useDeviceName

--- src/hooks/useDeviceName.ts
+++ src/hooks/useDeviceName.ts
@@ -1,16 +1,22 @@
 import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { useAppDispatch } from "redux/hooks";
-import { getDeviceName } from "redux/device/device-selectors";
+import { getDeviceName, getDeviceUID } from "redux/device/device-selectors";
 import { fetchDeviceInformation } from "redux/device/device-operations";
+import { api } from "services/omnic-api";
 
 const useDeviceName = () => {
+  const deviceUID = useSelector(getDeviceUID);
   const deviceName = useSelector(getDeviceName);
   const dispatch = useAppDispatch();
 
   useEffect(() => {
-    dispatch(fetchDeviceInformation());
-  }, [dispatch]);
+    api.defaults.headers.common["Authorization"] = deviceUID;
+
+    if (deviceUID) {
+      dispatch(fetchDeviceInformation());
+    }
+  }, [deviceUID, dispatch]);
 
   return { deviceName };
 };
