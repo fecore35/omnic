c91469a11eaff1914c119f45915ef7ef704794f3
fecore35 <79923560+fecore35@users.noreply.github.com>
2023-06-26 20:01:28 +0300

    check if size is not available

--- src/components/CellItem/CellItem.tsx
+++ src/components/CellItem/CellItem.tsx
@@ -18,16 +18,22 @@ const CellItem: FunctionComponent<CellItemProps> = ({ cell }) => {
   const title = findCellTitleBySize(type).toLowerCase();
 
   return (
-    <li className={`${s.cell} cell--${title}`}>
-      <div className={`${s.image} image--${title}`}>
-        <img src={images[`cell-${title}.svg`]} alt={type} />
-      </div>
-      <div>
-        <h3 className={s.title}>{title}</h3>
-        <p className={s.size}>
-          {normalizeSize(params.width)}x{normalizeSize(params.height)} см
-        </p>
-      </div>
+    <li className={`${s.cell} cell--${title} `} data-has_empty={cell.has_empty}>
+      <button
+        className={`${s.btn} ${!cell.has_empty ? s.disabled : ""}`}
+        type="button"
+        disabled={!cell.has_empty}
+      >
+        <div className={`${s.image} image--${title}`}>
+          <img src={images[`cell-${title}.svg`]} alt={type} />
+        </div>
+        <div>
+          <h3 className={s.title}>{title}</h3>
+          <p className={s.size}>
+            {normalizeSize(params.width)}x{normalizeSize(params.height)} см
+          </p>
+        </div>
+      </button>
     </li>
   );
 };
